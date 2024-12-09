26bd8a96282189beba2b0b62f0c7e04dc894f528
TommyTeaVee <fihlatv@gmail.com>
2024-12-09 18:34:43 +0200

    fixed admin token

--- Backend/index.js
+++ Backend/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 const adminRoutes = require('./src/routes/adminRoutes');
 const learnerRoutes = require('./src/routes/learnerRoutes');
-
+const { authenticateAdminToken, authorizeRole } = require('../middleware/authMiddleware');
 const app = express();
 
 // Middleware
